2ead4e926142afbc24d84ceba0c34cd7cf9c500d
Abinash <asavisingh72@gmail.com>
2024-01-03 19:23:04 +0530

    final commit

---
+++ xcounterclass/src/App.jsx
@@ -0,0 +1,40 @@
+
+import './App.css';
+
+import React, { Component } from "react";
+
+class CounterApp extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    };
+  }
+
+  increment = () => {
+    this.setState((prevState) => ({
+      count: prevState.count + 2,
+    }));
+  };
+
+  decrement = () => {
+    this.setState((prevState) => ({
+      count: prevState.count - 1,
+    }));
+  };
+
+  render() {
+    const { count } = this.state;
+
+    return (
+      <div>
+        <h1>Counter App</h1>
+        <p>Count: {count}</p>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+      </div>
+    );
+  }
+}
+
+export default CounterApp;
